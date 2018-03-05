cd7f1c373b7d96201fa5ecaa9aa63222da5e6545
hjb23 <harryjbeckwith@gmail.com>
2018-03-05 15:08:12 +0000

    first commit

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,34 @@
+<!--looping over quotes
+which are in the parent - app.vue
+placing the quote data inside-->
+<template>
+    <div class="row">
+        <app-quote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)">{{ quote }}</app-quote>
+    </div>
+</template>
+
+<script>
+    // import the quote component
+    // set props so it can be passed to parent (app.vue) and quote data can be accessed there
+    // set the component name
+
+
+    import Quote from './Quote.vue';
+
+    export default {
+        props: ['quotes'],
+        components: {
+            appQuote: Quote
+        },
+        methods: {
+            deleteQuote(index) {
+                this.$emit('quoteDeleted', index);
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
